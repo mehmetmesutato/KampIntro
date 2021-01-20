6ba2b758b548dea64595130b2d16df6ccf46d527
mehmetmesutato <mesut.mma@gmail.com>
2021-01-20 18:20:55 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştirme Kampı", "Programlama İçin Temel Kurs", "Java" };
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("Sayfa Sonu - Footer");
+
+            /*for (int i = 0; i < 10; i+=2)
+            {
+                Console.WriteLine(i);
+            }*/
+
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.65;
+            double DolarBugun = 7.45;
+
+            if (dolarDun < DolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun > DolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+        }
+    }
+}
