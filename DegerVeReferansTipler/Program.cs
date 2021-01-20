6c74b2b094a1294155a260f9c6ce5c901d2b3a20
mehmetmesutato <mesut.mma@gmail.com>
2021-01-20 18:37:17 +0300

    16 Ocak Ders Sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -7,7 +7,7 @@ namespace DegerVeReferansTipler
         static void Main(string[] args)
         {
             int sayi1 = 10;
-            int sayi2 = 40;
+            int sayi2 = 30;
             sayi1 = sayi2;
             sayi2 = 65;
 
