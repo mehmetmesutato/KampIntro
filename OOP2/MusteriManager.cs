6ed7540e5b2a71e01ab9907c8683a9d8d127cdd8
mehmetmesutato <mesut.mma@gmail.com>
2021-01-29 17:07:35 +0300

    20,23 Ocak Ders Sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class MusteriManager
+    {
+        //İş Sınıfları - Business Classes
+        public void Ekle(Musteri musteri) 
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+    }
+}
